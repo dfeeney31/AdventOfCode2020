bc4c95b6ddf416648fd708cf0e5a7ba88e0b4e98
Dan Feeney <daniel.feeney@colorado.edu>
2020-12-06 12:02:22 -0700

    day 3 work added

---
+++ day3.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 11:33:13 2020
+
+@author: Daniel.Feeney
+"""
+
+with open("C:/Users/Daniel.Feeney/Desktop/AdventOfCode2020/day3Input.txt", 'r') as f:
+    lines=f.readlines()
+
+# parameters        
+currentX = 0
+slopeX = 3
+slopeY = 1
+
+## Each line is 32 long, so each time you get to the end, the next x index is
+## 0 + remainder 
+
+# preallocate trees
+trees = 0
+for currentY, currentLine in enumerate(lines):
+    if currentLine[currentX] == "#":
+        trees+=1
+    currentX = (currentX + slopeX) % len(currentLine[:-1])
+print(f"Solution 1:{trees}")
+
